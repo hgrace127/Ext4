d294b5286402ac9a327bd663a5f064c954ae60d8
kwonyeseong <21200050@handong.edu>
2020-07-01 11:03:13 +0900

    [TEMP] Node.cpp

--- src/Node.cpp
+++ src/Node.cpp
@@ -1,3 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+enum NodeType{
+    None, 
+    File, 
+    Directory, 
+    HardLink,
+    SoftLink
+};
+
+enum NodeState
+{
+    Active, // Active 우선 구현
+    Deleted, 
+    Unused
+};
+
+enum NodeAttr
+{
+    Normal,
+    Container,
+    ContainerPart,
+    RecycleBin,
+    Virtual,
+    Recovered
+};
+
 class Node{
+    public:
+        long m_uid;
+        long m_gid;
+        unsigned short m_filemode;
+        long m_size;
+        long m_allocSize;
+        unsigned long m_refId;
+        int m_dumpId;
+
+    private:
+        string m_absolutePath; // 절대경로
+        string m_name; 
+        string m_extension; // 확장자
+        string m_partitionName;
+
 
 };
