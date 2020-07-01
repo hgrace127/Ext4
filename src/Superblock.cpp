1215dad231101fb10a89d4ab4c275d8ab16eeba4
seungju <hgrace127@naver.com>
2020-07-01 14:12:19 +0900

    [Error] pointer being freed was not allocated

--- src/Superblock.cpp
+++ src/Superblock.cpp
@@ -1,5 +1,9 @@
-using namespace std;
-#include <string>
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include "develop/include/byte_buffer2.hpp"
+#include "develop/include/endian_swap.hpp"
+
 class Superblock{
     public :
     
@@ -7,42 +11,53 @@ class Superblock{
             return magicSignature == 0xef53 && iNodeSize != 0 && blkPerGroup !=0;
         };
         
-        // unsigned int : unsigned int
-        // unsigned short : unsigned short
-        // unsigned char : unsigned char
+        // uint32_t : uint32_t
+        // uint16_t : uint16_t
+        // uint8_t : uint8_t
         
-        void SuperBlock(unsigned char* b, int offset = 0){ 
-            iNodeCnt                            = *reinterpret_cast<unsigned int*>(b + offset+0); 
-            blkCnt                              = *reinterpret_cast<unsigned int*>(b + offset+4); 
-            reservedBlkCnt                      = *reinterpret_cast<unsigned int*>(b + offset+8); 
-            freeBlkCnt                          = *reinterpret_cast<unsigned int*>(b + offset+12); 
-            freeiNodeCnt                        = *reinterpret_cast<unsigned int*>(b + offset+16); 
-            firstDataBlk                    = *reinterpret_cast<unsigned int*>(b + offset+20); 
-            logBlkSize                      = *reinterpret_cast<unsigned int*>(b + offset+24); 
-            logFragmentationSize            = *reinterpret_cast<unsigned int*>(b + offset+28); 
-            blkPerGroup                 = *reinterpret_cast<unsigned int*>(b + offset+32); 
-            fragmentationPerGroup                 = *reinterpret_cast<unsigned int*>(b + offset+36); 
-            iNodePerGroup                 = *reinterpret_cast<unsigned int*>(b + offset+40); 
-            mTime                 = *reinterpret_cast<unsigned int*>(b + offset+44); 
-            wTime                 = *reinterpret_cast<unsigned int*>(b + offset+48); 
-            mountCnt                 = *reinterpret_cast<unsigned int*>(b + offset+52); 
-            nextMountCnt                 = *reinterpret_cast<unsigned int*>(b + offset+54); 
-            magicSignature                 = *reinterpret_cast<unsigned int*>(b + offset+56); 
-            state                 = *reinterpret_cast<unsigned int*>(b + offset+58); 
-            errors                 = *reinterpret_cast<unsigned int*>(b + offset+60); 
-            minorVersion                 = *reinterpret_cast<unsigned int*>(b + offset+62); 
-            lastConsistencyCheckTime                 = *reinterpret_cast<unsigned int*>(b + offset+64); 
-            checkInterval                 = *reinterpret_cast<unsigned int*>(b + offset+68); 
-            createOS                 = *reinterpret_cast<unsigned int*>(b + offset+72);
-            majorVersion                 = *reinterpret_cast<unsigned int*>(b + offset+76); 
-            UID                 = *reinterpret_cast<unsigned int*>(b + offset+80); 
-            GID                 = *reinterpret_cast<unsigned int*>(b + offset+82); 
-            firstNonReservediNode                 = *reinterpret_cast<unsigned int*>(b + offset+84); 
-            iNodeSize                 = *reinterpret_cast<unsigned int*>(b + offset+88);
-            blkGroupNo                 = *reinterpret_cast<unsigned int*>(b + offset+90); 
-            compatibleFeatureFlags                 = *reinterpret_cast<unsigned int*>(b + offset+92); 
-            incompatibleFeatureFlags                 = *reinterpret_cast<unsigned int*>(b + offset+96); 
-            readOnlyFeatureFlags                 = *reinterpret_cast<unsigned int*>(b + offset+100); 
+        Superblock(uint8_t* b, int offset = 0){ 
+            ByteBuffer2 bb(b, offset, 1024, true);
+            // ByteBuffer2 bb_ushort;
+            std::cout <<"11" <<'\n';
+            iNodeCnt                            = bb.get_uint32_le();  // 총 아이노드 수
+            reservedBlkCnt                      = bb.get_uint32_le();  // 총 블록 수
+            freeBlkCnt                          = bb.get_uint32_le(); 
+            freeiNodeCnt                        = bb.get_uint32_le(); 
+            firstDataBlk                        = bb.get_uint32_le(); 
+            logBlkSize                          = bb.get_uint32_le(); 
+            logFragmentationSize                = bb.get_uint32_le(); 
+            blkPerGroup                         = bb.get_uint32_le();   // 그룹 당 블록 수 ?
+            fragmentationPerGroup               = bb.get_uint32_le();   
+            iNodePerGroup                       = bb.get_uint32_le();   // 그룹 당 아이노드 ?
+            mTime                               = bb.get_uint32_le(); 
+            wTime                               = bb.get_uint32_le(); 
+            std::cout <<"22" <<'\n';
+            mountCnt                            = bb.get_uint16_le(); 
+            nextMountCnt                        = bb.get_uint16_le(); 
+            magicSignature                      = bb.get_uint16_le(); //Debug.Assert(magicSignature == 0xef53);
+            state                               = bb.get_uint16_le(); 
+            errors                              = bb.get_uint16_le(); 
+            minorVersion                        = bb.get_uint16_le(); 
+            lastConsistencyCheckTime            = bb.get_uint32_le();
+            checkInterval                       = bb.get_uint32_le();
+            createOS                            = bb.get_uint32_le();
+            majorVersion                        = bb.get_uint32_le();
+            UID                                 = bb.get_uint16_le();
+            GID                                 = bb.get_uint16_le();
+            firstNonReservediNode               = bb.get_uint32_le();
+            iNodeSize                           = bb.get_uint16_le();   // 아이노드 사이즈
+            blkGroupNo                          = bb.get_uint16_le();   // 수퍼블록의 블록 그룹 번호
+            compatibleFeatureFlags              = bb.get_uint32_le();
+            incompatibleFeatureFlags            = bb.get_uint32_le();
+            readOnlyFeatureFlags                = bb.get_uint32_le();
+            std::cout <<"22" <<'\n';
+
+            // UUID                                = new uint8_t[16];
+            // memcpy(UUID, b+offset+104, 16); // ?
+            // volumeName                          = new uint8_t[16];
+            // memcpy(volumeName, b+offset+120, 16);
+            bb.destroy();
+
 
         }
 
@@ -50,60 +65,60 @@ class Superblock{
             return (incompatibleFeatureFlags & 0x80) == 0x80 ? padding3 : 0x20;
         }
 
-        unsigned int ActiveiNodeNo(){
+        uint32_t ActiveiNodeNo(){
             return iNodeCnt - freeiNodeCnt;
         }
         
 
-
-        unsigned int  iNodeCnt;
-        unsigned int  blkCnt;
-        unsigned int  reservedBlkCnt;
-        unsigned int  freeBlkCnt;
-        unsigned int  freeiNodeCnt;
-        unsigned int  firstDataBlk;
-        unsigned int  logBlkSize;
-        unsigned int  logFragmentationSize;
-        unsigned int  blkPerGroup;
-        unsigned int  fragmentationPerGroup;
-        unsigned int  iNodePerGroup;
-        unsigned int  mTime;          // last mount time
-        unsigned int  wTime;          // super block update time
-        unsigned short mountCnt;
-        unsigned short nextMountCnt;
-        unsigned short magicSignature;
-        unsigned short state;
-        unsigned short errors;
-        unsigned short minorVersion;
-        unsigned int  lastConsistencyCheckTime;
-        unsigned int  checkInterval;
-        unsigned int  createOS;
-        unsigned int  majorVersion;
-        unsigned short UID;                    //  UID for reserved block
-        unsigned short GID;                    //  GID for reserved block
-        unsigned int  firstNonReservediNode;  //  GID for reserved block
-        unsigned short iNodeSize;
-        unsigned short blkGroupNo;
-        unsigned int  compatibleFeatureFlags;
-        unsigned int  incompatibleFeatureFlags;
-        unsigned int  readOnlyFeatureFlags;
-        unsigned char* UUID;
-        unsigned char* volumeName;
-        unsigned char* lastMountedPath;
-        unsigned int  algorithmUsageBitmap;
-        unsigned char   preAllocatedBlkCount;
-        unsigned char   preAllocatedDirBlkCount;
-        unsigned short padding1;
-        unsigned char* journalUUID;
-        unsigned int  journaliNodeNo;
-        unsigned int  journalDevice;
-        unsigned int  journalOrphaniNodeList;
-        unsigned char* hashSeed;
-        unsigned char   definedHashVersion;
-        unsigned char   padding2;
-        unsigned short padding3;
-        unsigned int  defaultMountOption;
-        unsigned int  firstMetaBlock;
+    
+        uint32_t  iNodeCnt;
+        uint32_t  blkCnt;
+        uint32_t  reservedBlkCnt;
+        uint32_t  freeBlkCnt;
+        uint32_t  freeiNodeCnt;
+        uint32_t  firstDataBlk;
+        uint32_t  logBlkSize;
+        uint32_t  logFragmentationSize;
+        uint32_t  blkPerGroup;
+        uint32_t  fragmentationPerGroup;
+        uint32_t  iNodePerGroup;
+        uint32_t  mTime;          // last mount time
+        uint32_t  wTime;          // super block update time
+        uint16_t mountCnt;
+        uint16_t nextMountCnt;
+        uint16_t magicSignature;
+        uint16_t state;
+        uint16_t errors;
+        uint16_t minorVersion;
+        uint32_t  lastConsistencyCheckTime;
+        uint32_t  checkInterval;
+        uint32_t  createOS;
+        uint32_t  majorVersion;
+        uint16_t UID;                    //  UID for reserved block
+        uint16_t GID;                    //  GID for reserved block
+        uint32_t  firstNonReservediNode;  //  GID for reserved block
+        uint16_t iNodeSize;
+        uint16_t blkGroupNo;
+        uint32_t  compatibleFeatureFlags;
+        uint32_t  incompatibleFeatureFlags;
+        uint32_t  readOnlyFeatureFlags;
+        uint8_t* UUID;
+        uint8_t* volumeName;
+        uint8_t* lastMountedPath;
+        uint32_t  algorithmUsageBitmap;
+        uint8_t   preAllocatedBlkCount;
+        uint8_t   preAllocatedDirBlkCount;
+        uint16_t padding1;
+        uint8_t* journalUUID;
+        uint32_t  journaliNodeNo;
+        uint32_t  journalDevice;
+        uint32_t  journalOrphaniNodeList;
+        uint8_t* hashSeed;
+        uint8_t   definedHashVersion;
+        uint8_t   padding2;
+        uint16_t padding3;
+        uint32_t  defaultMountOption;
+        uint32_t  firstMetaBlock;
         // reserved 760 bytes
 
 };
