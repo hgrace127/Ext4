f14f96e2e5da8f36229cfc92112db3adb48ef61c
kwonyeseong <21200050@handong.edu>
2020-07-01 13:37:50 +0900

    [UPDATE] add variables

--- src/Ext4.cpp
+++ src/Ext4.cpp
@@ -1,3 +1,57 @@
+#include <vector>
+
+#include "Node.cpp"
+#include "Superblock.cpp"
+#include "BlockGroupDescriptor.cpp"
+
+enum BlockType
+{
+    Boot,
+    Super,
+    GroupDesc,
+    BlockBitmap,
+    InodeBitmap,
+    InodeTable,
+    Data,
+    Invalid
+};
+
+class Extent{
+public:
+    long m_LOffset;
+    long m_POffset;
+    long m_count;
+
+private:
+
+public:
+    Extent() {}
+
+};
+
+
 class Ext4{
+
+public:
+    long m_size;
+    int m_blockSize;
+    int m_iNodeSize;
+
+    Node m_rootNode;   
+
+private:
+    Superblock m_superblock;
+    vector<BlockGroupDescriptor> m_blkGroupDescTable;
+    int m_blockGroupCount;
+    int m_iNodePerBlock; // 한 블록이 들어갈 수 있는 INode 갯수
+    int m_iNodeBlockCount;
+
+    bool m_isLive;
+    long m_startAddress;
+
+public:
+    auto BuildFileSystem() {}
+
+
 
 };
--- src/Node.cpp
+++ src/Node.cpp
@@ -28,20 +28,47 @@ enum NodeAttr
 };
 
 class Node{
-    public:
-        long m_uid;
-        long m_gid;
-        unsigned short m_filemode;
-        long m_size;
-        long m_allocSize;
-        unsigned long m_refId;
-        int m_dumpId;
+public:
+    long m_uid;
+    long m_gid;
+    unsigned short m_filemode;
+    long m_size;
+    long m_allocSize;
+    unsigned long m_refId;
+    int m_dumpId;
+    NodeList* m_children;
+    Node* m_parent;
+    NodeAttr m_nodeAttr;
 
-    private:
-        string m_absolutePath; // 절대경로
-        string m_name; 
-        string m_extension; // 확장자
-        string m_partitionName;
+private:
+    string m_absolutePath; // 절대경로
+    string m_name; 
+    string m_extension; // 확장자
+    string m_partitionName;
+    long m_offset;
+    NodeType m_nodeType;
+    
+public:
+    Node() {}
+    ~Node() {}
 
+private:
+    auto getBytes(string value, string as) -> uint8_t*
+    {}
+        
+    
+};
+
+class NodeList{
+public:
+    int m_count;
+private:
+    int m_activeNodeIndex;
+    vector<Node>* m_nodes;
+    Node* m_owner; // parent node를 말하는 듯
+
+public:
+    NodeList() {}
+    ~NodeList() {}
 
 };
