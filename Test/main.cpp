78cf0051a0421f75eefb21c690970ca32f870ff2
seungju <hgrace127@naver.com>
2020-07-01 17:09:35 +0900

    [Update] CMakeLists, main

--- Test/main.cpp
+++ Test/main.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
+#include <fstream>
+#include "develop/include/byte_buffer2.hpp"
+#include "develop/include/endian_swap.hpp"
+#include "../src/Superblock.cpp"
+#include <cstring>
 
 using namespace std;
 
 int main(){
-    cout << "hello world!" << endl;
+    ifstream in("../data/ext4.bin", ios::binary);
     
+    uint8_t data[1024];
+    if(in.is_open()){
+        in.seekg(0x400);
+        in.read((char*)data, 0x400);
+        cout << hex << data << '\n';
+    }else {
+        cout << "error" << '\n';
+    }
+    
+    Superblock *sb = new Superblock(data, 0);
+    cout << "11" << '\n';
+    // cout << sb->iNodeCnt << '\n';
+    // cout << sb->magicSignature << '\n';
+    // cout << sb->iNodeSize << '\n';
+    // cout << sb->blkGroupNo << '\n';
+
     return 0;
 }
